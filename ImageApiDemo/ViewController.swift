4105d04150885c04c4380d36abebd9bbcf76a969
russell-archer <russell.archer@mac.com>
2018-06-18 16:10:30 +0100

    Initial Commit

---
+++ ImageApiDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageApiDemo
+//
+//  Created by Russell Archer on 18/06/2018.
+//  Copyright © 2018 Russell Archer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
